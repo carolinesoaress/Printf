030fb995f87eaa9252271e0f312396e84bdd3a0c
Caroline Soares <104658660+carolinesoaress@users.noreply.github.com>
2023-12-12 18:57:24 -0300

    Add files via upload
    ajustando o retorno do ft_printf

--- ft_printf.c
+++ ft_printf.c
@@ -4,15 +4,17 @@
 int	ft_printf(const char *string, ...)
 {
   int num_args = ft_strlen(string);
-  int x, i;
+  int x, i, n;
   va_list args;
   va_start(args, string);
   
   i = 0;
+  n = 0;
   while (i < num_args)
   {
     if (string[i] == '%' && string[i + 1] == 'd')
     {
+        n++;
         x = va_arg(args, int);
         ft_putnbr(x);
         i++;
@@ -21,14 +23,21 @@ int	ft_printf(const char *string, ...)
         ft_putchar(string[i]);
     i++;
   }
+  return (i - n);
 }
 
 int main(void)
 {
     int n = 4;
     int m = 3; 
-   
-    ft_printf("O num %d é top e o %d é ok\n", n, m);
+
+    int total, original;
+
+    total = ft_printf("O num %d é legal o %d é ok\n", n, m);
+    ft_printf("%d\n", total);
+
+    original = printf("O num %d é legal o %d é ok\n", n, m);
+    printf("original: %d\n", original);  
 
     return (0);
 }
